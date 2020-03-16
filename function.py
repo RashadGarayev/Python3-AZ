329904f2094bdec037ca85cc3202f18d653f5b99
Rashad Garayev <pythonaz@yahoo.com>
2020-03-16 10:09:06 -0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,24 @@
+def toplama(x,y):
+    """
+    Funksiya toplama əməlini yerinə yetirir
+    """
+    result = x+y
+    return result
+def çıxma(x,y):
+    """
+    Funksiya çıxma əməlini yerinə yetirir
+    """
+    result = x-y 
+    return result
+def hasil(x,y):
+    """
+    Funksiya vurma əməlini yerinə yetirir
+    """
+    result = x*y 
+    return result
+def bölmə(x,y):
+    """
+    Funksiya bölmə əməlini yerinə yetirir
+    """
+    result = x/y 
+    return result
---
+++ test.py
@@ -0,0 +1,5 @@
+import function
+print(dir(function))
+print('-------------------------------------')
+
+print('Cavab:',function.hasil(12,4))
