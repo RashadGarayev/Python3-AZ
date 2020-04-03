4d4216e4c76edbe358e5ff26739426355ffc2070
Rashad Garayev <pythonaz@yahoo.com>
2020-04-03 12:45:58 -0700

    update

--- GUI/qpixmap.py
+++ GUI/qpixmap.py
@@ -4,7 +4,6 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import sys, webbrowser
 
-
 class MainWindow(QWidget):
 
     def __init__(self, parent=None):
@@ -18,28 +17,11 @@ class MainWindow(QWidget):
         self.palette.setColor(QPalette.Background, Qt.white)
         self.setPalette(self.palette)
         self.setGeometry(600,150,512,512)
-        self.label = QLabel(self)
-        self.label.setText(u'Adınız')
-        self.label.move(150,50)
-
-        self.lineedit = QLineEdit(self)
-        self.lineedit.setAlignment(Qt.AlignLeft)
-        self.lineedit.move(200, 50)
-
-        self.label1 = QLabel(self)
-        self.label1.setText((u'Yaşınız'))
-        self.label1.move(150,80)
-        self.lineedit1 = QLineEdit(self)
-        self.lineedit1.setInputMask('99')
-        self.lineedit1.setAlignment(Qt.AlignLeft)
-        self.lineedit1.move(200,80)
         self.button = QPushButton(self)
-        self.button.setText(u'click')
-        self.button.move(230,100)
-        self.button.clicked.connect(self.goster)
+        self.button.setText(u'Başla')
+        self.button.move(220,50)
         self.show()
-    def goster(self):
-        print('Adım :{} | Yaşım :{}'.format(self.lineedit.text(),self.lineedit1.text()))
+
 
 if __name__ == '__main__':
     app = QApplication([])
