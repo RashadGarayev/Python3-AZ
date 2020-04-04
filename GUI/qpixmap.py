2b403621d3c943b6b9109ba5df7192cea3fc2371
Rashad Garayev <pythonaz@yahoo.com>
2020-04-04 11:10:27 -0700

    gui

--- GUI/qpixmap.py
+++ GUI/qpixmap.py
@@ -2,14 +2,13 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-import sys, time
-var1 =time.time()
+import sys, webbrowser
+
 class MainWindow(QWidget):
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         # Əsas pəncərə self ifadəsində referans alındı.
-
         self.setWindowTitle("Bu mənim ilk proqramım")
         # Pəncərə loqosu
         self.setWindowIcon(QIcon('../image/ico.png'))  # Pəncərə sol yuxarı küncdə loqo əlavə olundu
@@ -17,23 +16,19 @@ class MainWindow(QWidget):
         self.palette = QPalette()
         self.palette.setColor(QPalette.Background, Qt.white)
         self.setPalette(self.palette)
-        self.setGeometry(600,150,512,512)
-        self.button = QPushButton(self)
-        self.button.setText(u'Başla')
-        self.button.setIcon(QIcon('../image/bstart.png'))
-        self.button.move(220,50)
-        self.button.clicked.connect(self.Speed)
+        self.spinbox = QSpinBox()
+        self.label = QLabel('Spinbox value:')
+        self.spinbox.resize(100, 25)
+        self.spinbox.setRange(1994, 2009)
+        self.spinbox.setValue(200)
+        self.grid = QGridLayout(self)
+        self.grid.addWidget(self.spinbox)
+        self.grid.addWidget(self.label)
+        self.spinbox.valueChanged.connect(self.create)
         self.show()
-    def Speed(self):
-        var2 = time.time()
-        delta = var1-var2
-        self.label = QLabel(self)
-        self.label.setText('{} saniyə'.format(str(delta)))
-        self.label.move(220,80)
-        self.label.show()
 
-
-
+    def create(self):
+        self.label.setText('Value:' + str(self.spinbox.value()))
 if __name__ == '__main__':
     app = QApplication([])
     gui = MainWindow()
